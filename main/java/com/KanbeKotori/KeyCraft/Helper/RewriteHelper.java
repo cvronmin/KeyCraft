3e43a641954417b51246990b48a66dc1b78274b0
noname00 <xfgryujk@126.com>
2015-11-03 18:04:20 +0800

    整理代码

--- main/java/com/KanbeKotori/KeyCraft/Helper/RewriteHelper.java
+++ main/java/com/KanbeKotori/KeyCraft/Helper/RewriteHelper.java
@@ -12,13 +12,54 @@ import com.KanbeKotori.KeyCraft.Helper.*;
 
 public class RewriteHelper {
 	
+	public static class Skill {
+		public final int id;
+		public final int AuroraRequired;
+		public Skill(int id, int AuroraRequired) {
+			this.id = id;
+			this.AuroraRequired = AuroraRequired;
+		}
+	}
+	
+	public static final Skill AuroraCognition		= new Skill(000, 0);
+	public static final Skill UrgentProtect			= new Skill(200, 5);
+	public static final Skill BattleReadiness		= new Skill(211, 25);
+	public static final Skill PhysiqueUp			= new Skill(212, 40);
+	public static final Skill FireAttach			= new Skill(221, 10);
+	public static final Skill PoisonAttach			= new Skill(222, 10);
+	public static final Skill WitherAttach			= new Skill(223, 20);
+	public static final Skill SuperVibration		= new Skill(231, 10);
+	public static final Skill BruteForce			= new Skill(232, 10);
+	public static final Skill WeaponsProficient		= new Skill(233, 30);
+	public static final Skill LifeSuck				= new Skill(241, 20);
+	public static final Skill PowerDown				= new Skill(242, 20);
+	public static final Skill AuroraRob				= new Skill(243, 20);
+	public static final Skill AuroraControl			= new Skill(300, 5);
+	public static final Skill AuroraTrident			= new Skill(311, 5);
+	public static final Skill AuroraBlade			= new Skill(312, 10);
+	public static final Skill FireResist			= new Skill(321, 5);
+	public static final Skill FireResistUp			= new Skill(322, 5);
+	public static final Skill FireResistMax			= new Skill(323, 20);
+	public static final Skill ExplosionResist		= new Skill(331, 10);
+	public static final Skill MagicResist			= new Skill(332, 10);
+	public static final Skill UltimateHardening		= new Skill(333, 30);
+	public static final Skill AuroraActivation		= new Skill(341, 20);
+	public static final Skill AuroraSurge			= new Skill(342, 20);
+	public static final Skill AuroraRegeneration	= new Skill(343, 40);
+	
+	public static final Skill SKILLS[] = { AuroraCognition, UrgentProtect, UrgentProtect, BattleReadiness, PhysiqueUp, FireAttach, PoisonAttach, 
+			WitherAttach, SuperVibration, BruteForce, WeaponsProficient, LifeSuck, PowerDown, AuroraRob, AuroraControl, AuroraTrident, 
+			AuroraBlade, FireResist, FireResistUp, FireResistMax, FireResistMax, ExplosionResist, MagicResist, UltimateHardening, 
+			AuroraActivation, AuroraSurge, AuroraRegeneration };
+	
+	
 	public static void setPoint_First(EntityPlayer player) {
 		setAuroraPoint(player, 100);
-		setPoint(player, 000, true);
+		setPoint(player, AuroraCognition.id, true);
 	}
 	
 	public static boolean hasFirstSet(EntityPlayer player) {
-		return getPoint(player, 000);
+		return getPoint(player, AuroraCognition.id);
 	}
 	
 	public static void setAuroraPoint(EntityPlayer player, int point) {
@@ -54,41 +95,18 @@ public class RewriteHelper {
 	}
 	
 	public static int getAuroraRequired(int skill) {
-		switch (skill) {
-			case 200:	return 5;
-			case 211:	return 25;
-			case 212:	return 40;
-			case 221:	return 10;
-			case 222:	return 10;
-			case 223:	return 20;
-			case 231:	return 10;
-			case 232:	return 10;
-			case 233:	return 30;
-			case 241:	return 20;
-			case 242:	return 20;
-			case 243:	return 20;
-			case 300:	return 5;
-			case 311:	return 5;
-			case 312:	return 10;
-			case 321:	return 5;
-			case 322:	return 5;
-			case 323:	return 20;
-			case 331:	return 10;
-			case 332:	return 10;
-			case 333:	return 30;
-			case 341:	return 20;
-			case 342:	return 20;
-			case 343:	return 40;
-			default:	return 0x7FFFFFFF;	
+		for (Skill i : SKILLS) {
+			if (i.id == skill) {
+				return i.AuroraRequired;
+			}
 		}
+		return 0x7FFFFFFF;
 	}
 	
 	public static void CLONE(EntityPlayer _old, EntityPlayer _new) {
 		setAuroraPoint(_new, getAuroraPoint(_old));
-		final int a[] = {000, 200, 211, 212, 221, 222, 223, 231, 232, 233, 241, 242, 243, 300, 311, 312, 321, 322, 
-						 323, 331, 332, 333, 341, 342, 343};
-		for (int i = 0; i < 25; i++) {
-			setPoint(_new, a[i], getPoint(_old, a[i]));
+		for (Skill i : SKILLS) {
+			setPoint(_new, i.id, getPoint(_old, i.id));
 		}
 	}
 	
